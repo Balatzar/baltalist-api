9417cc4e302d14c45e76bddb9fa2a12fd0fd5d35
balatzar <bataprod@gmail.com>
2019-11-24 18:10:31 +0100

    Log db url

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const { Pool } = require("pg");
+console.log(process.env.DATABASE_URL);
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool(
   connectionString ? connectionString : { database: "baltalist" }
