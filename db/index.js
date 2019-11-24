0073de1bf1a4d639511e27286cfc069165aaa2f3
balatzar <bataprod@gmail.com>
2019-11-24 18:04:21 +0100

    Add postgres prod

--- db/index.js
+++ db/index.js
@@ -1,5 +1,9 @@
 const { Pool } = require("pg");
-const pool = new Pool({ database: "baltalist" });
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool(
+  connectionString ? connectionString : { database: "baltalist" }
+);
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
